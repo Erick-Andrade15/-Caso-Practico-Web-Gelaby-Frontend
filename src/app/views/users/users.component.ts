157b9d0aac0c129063493a3ad945654bfe3a1d7d
Erick-Andrade15 <erickandrade15a@gmail.com>
2023-06-07 09:58:54 -0500

    Export - PDF - Users

--- src/app/views/users/users.component.ts
+++ src/app/views/users/users.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { UsersModel } from 'src/app/models/users.model';
 import { UsersService } from 'src/app/services/users.service';
 import Swal from 'sweetalert2';
+import * as jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent {
   title: string = 'Usuarios';
@@ -46,9 +48,7 @@ export class UsersComponent {
         this.apiService.deleteUser(id).subscribe(
           () => {
             // Eliminar el elemento del arreglo de usuarios
-            this.Users = this.Users.filter(
-              (user) => user.user_id !== id
-            );
+            this.Users = this.Users.filter((user) => user.user_id !== id);
             // Mostrar una alerta de éxito después de eliminar
             Swal.fire({
               icon: 'success',
@@ -73,7 +73,58 @@ export class UsersComponent {
       }
     });
   }
-  
 
+  exportToPDF() {
+    if (this.Users.length <= 0) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'No hay usuarios disponibles para exportar.',
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      return;
+    }
+    // Crea una nueva instancia de jsPDF
+    const doc = new jsPDF.default();
 
+    // Agrega el título al PDF
+    const title = 'Reporte de Usuarios';
+    const fontSize = 30;
+    const pageWidth = doc.internal.pageSize.getWidth();
+    const textWidth =
+      (doc.getStringUnitWidth(title) * fontSize) / doc.internal.scaleFactor;
+    const titleX = (pageWidth - textWidth) / 2;
+    const titleY = 20;
+
+    doc.setFontSize(fontSize);
+    doc.text(title, titleX, titleY);
+
+    // Obtén los datos de la tabla
+    const tableData = this.Users.map((user, index) => [
+      index + 1,
+      user.user_fullname,
+      user.user_username,
+      user.user_email,
+      user.user_role,
+    ]);
+
+    // Define las columnas de la tabla
+    const headers = [
+      ['Nº', 'Nombre Completo', 'Usuario', 'Correo Institucional', 'Rol'],
+    ];
+
+    // Establece la posición inicial de la tabla
+    const startY = titleY + 10;
+
+    // Genera el contenido de la tabla
+    (doc as any).autoTable({
+      head: headers,
+      body: tableData,
+      startY: startY,
+    });
+
+    // Guarda el PDF con un nombre de archivo
+    doc.save('usuarios.pdf');
+  }
 }
